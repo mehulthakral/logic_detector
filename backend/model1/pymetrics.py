291ec42a80b68d6ba36c0096b1788b21945a33be
bhavnaaro <bhavnaaro28@gmail.com>
2021-02-08 11:45:08 +0530

    added cyclomatic for py

--- backend/model1/pymetrics.py
+++ backend/model1/pymetrics.py
@@ -1,3 +1,47 @@
+from radon.visitors import ComplexityVisitor
+from radon.raw import analyze
+
+def cyclomatic_complexity(fnc_str):
+    v = ComplexityVisitor.from_code(fnc_str)
+    allfunc = v.functions
+    #print(allfunc)
+    funcs={}
+    for f in allfunc:
+        funcs[f[0]]=f[-1]
+        # print(f[0], " ", f[-1])
+    return funcs
+
+def other_metrics(fnc_str):
+    stats = analyze(fnc_str)
+    return stats
+
+if __name__=="__main__":
+    
+
+    code_sample = """
+def factorial(n):
+    if n < 2: 
+        return 1
+    return n * factorial(n - 1)
+    
+def foo(bar):
+    #foo function
+    return sum(i for i in range(bar ** 2) if bar % i)
+
+def SORT(arr: list):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+        """
+
+    
+    print(other_metrics(code_sample))
+    print(cyclomatic_complexity(code_sample))
+
 
 # def factorial(n):
 #     if n < 2:
@@ -36,52 +80,3 @@
 # 	if arr[j] > arr[j+1]:
 # 	arr[j], arr[j+1] = arr[j+1], arr[j]
 # 	return arr
-
-from radon.visitors import ComplexityVisitor
-from radon.raw import analyze
-code_sample1 = """
-def _split_tokens(tokens, token, value):
-    '''Split a list of tokens on the specified token pair (token, value),
-    where *token* is the token type (i.e. its code) and *value* its actual
-    value in the code.
-    '''
-    res = [[]]
-    for token_values in tokens:
-        if (token, value) == token_values[:2]:
-            res.append([])
-            continue
-        res[-1].append(token_values)
-    return res
-"""
-code_sample2 ='''
-def factorial(n):
-    if n < 2: return 1
-    return n * factorial(n - 1)
-
-def foo(bar):
-    return sum(i for i in range(bar ** 2) if bar % i)
-
-def SORT(arr: list):
-    n = len(arr)
-
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-'''
-stat_analysis = analyze(code_sample1)
-v = ComplexityVisitor.from_code(code_sample2)
-
-print("Analysis:\n",stat_analysis,"\n")
-
-allfunc=v.functions
-print(allfunc)
-print("\nFUNC\tCYCLO_COMPLEXITY")
-for f in allfunc:
-    print(f[0]," ",f[-1])
-
-# [Function(name='factorial', lineno=2, col_offset=0, endline=4, is_method=False,
-#           classname=None, closures=[], complexity=2),
-#  Function(name='foo', lineno=6, col_offset=0, endline=7, is_method=False, classname=None,
-#           closures=[], complexity=3)]
